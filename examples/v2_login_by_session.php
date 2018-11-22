0a094cfa37b2a440ae301f9a0021204b9b8aafc1
Volodymyr Tsumanchuk <volodymyr.tsumanchuk@splynx.com>
2018-11-22 16:27:19 +0200

    SAPIPHP-2 Added more examples #time 20m

---
+++ examples/v2_login_by_session.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Splynx API v2.0 demo script
+ * Author: Volodymyr Tsumanchuk (Splynx s.r.o.)
+ * https://splynxv2rc.docs.apiary.io - API documentation
+ */
+
+include '../src/SplynxApi.php';
+
+$api_url = 'http://splynx/'; // please set your Splynx URL
+
+$session_id= "SESSION_ID"; // Splynx session id
+
+$api = new SplynxAPI($api_url);
+$api->setVersion(SplynxApi::API_VERSION_2);
+
+$isAuthorized = $api->login([
+    'auth_type' => SplynxApi::AUTH_TYPE_SESSION,
+    'session_id' => $session_id,
+]);
+
+if (!$isAuthorized) {
+    exit("Authorization failed!\n");
+}
+
+print "Authorization info: " . var_export($api->getAuthData(), 1) . "\n";
+
+$customersApiUrl = "admin/customers/customer";
+
+print "Get customers\n";
+$result = $api->api_call_get($customersApiUrl);
+print "Result: ";
+if ($result) {
+    print "Ok!\n";
+    print_r($api->response);
+} else {
+    print "Fail! Error code: $api->response_code\n";
+    print_r($api->response);
+}
+print "\n-------------------------------------------------\n";
